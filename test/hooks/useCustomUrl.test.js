3406e2f734d844f80a81ae4fbc9d4b3d1d1f0932
James Delorey <delorey@adobe.com>
2021-02-09 11:35:21 -0600

    test: DATA-2473 Adding tests for react hooks
    useCustomUrl, useLocation, useMagentoExtensionContext, usePageVisibility, useShoppingCartContext,
    useStorefrontInstanceContext, useViewedOffsets

--- test/hooks/useCustomUrl.test.js
+++ test/hooks/useCustomUrl.test.js
@@ -13,6 +13,7 @@ jest.mock('react-router-dom', () => ({
   useLocation: jest
     .fn()
     .mockReturnValueOnce(first)
+    .mockReturnValueOnce({ ...first })
     .mockReturnValueOnce(second)
     .mockReturnValueOnce(second),
 }));
@@ -26,6 +27,10 @@ test('custom url flow control', () => {
   const customUrlSpy = jest.fn();
   mdl.subscribe.customUrl(customUrlSpy);
   const { rerender } = renderHook(() => useCustomUrl());
+  expect(customUrlSpy).toHaveBeenCalledTimes(1);
+  expect(mdl.context.getCustomUrl().customUrl).toEqual(firstLocation);
+  // refiring useEffect without changing location doesn't publish event
+  rerender();
   expect(customUrlSpy).toHaveBeenCalledTimes(1);
   expect(mdl.context.getCustomUrl().customUrl).toEqual(firstLocation);
   // rerendering with new location fires events and updates context
---
+++ test/hooks/usePageVisibility.test.js
@@ -0,0 +1,49 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import usePageVisibility from '../../lib/hooks/usePageVisibility';
+
+const first = {
+  pathname: '/first',
+  key: 'foo',
+};
+const second = {
+  pathname: 'second',
+};
+
+jest.mock('react-router-dom', () => ({
+  useLocation: jest
+    .fn()
+    .mockReturnValueOnce(first)
+    .mockReturnValueOnce({ ...first, key: 'bar' })
+    .mockReturnValueOnce(second)
+    .mockReturnValueOnce(second),
+}));
+
+test('creates and removes page visibility listeners', () => {
+  const oldAdd = document.addEventListener;
+  const oldRemove = document.removeEventListener;
+  document.addEventListener = jest.fn();
+  document.removeEventListener = jest.fn();
+
+  const { unmount } = renderHook(() => usePageVisibility());
+  expect(document.addEventListener).toHaveBeenCalledTimes(1);
+  unmount();
+  expect(document.removeEventListener).toHaveBeenCalledTimes(1);
+
+  document.addEventListener = oldAdd;
+  document.removeEventListener = oldRemove;
+});
+
+test('handles visibility change', () => {
+  delete document.visibilityState;
+  Object.defineProperty(document, 'visibilityState', {
+    value: 'visible',
+    writable: true,
+  });
+  const { result } = renderHook(() => usePageVisibility());
+  expect(result.current.isVisible).toBe(true);
+  document.visibilityState = 'hidden';
+  act(() => {
+    document.dispatchEvent(new Event('visibilitychange'));
+  });
+  expect(result.current.isVisible).toBe(false);
+});
