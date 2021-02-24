b2c210dbf1a4b1984f23d6179350469aa6b70015
James Delorey <delorey@adobe.com>
2021-02-24 16:50:14 -0600

    refactor: DATA-2844 refactor and test sign in hooks

---
+++ test/wrappers/wrapUseSignIn.test.js
@@ -0,0 +1,22 @@
+import { renderHook } from '@testing-library/react-hooks';
+import wrapUseSignIn from '../../lib/collector/wrappers/wrapUseSignIn';
+import mdl from 'magento-data-layer-sdk';
+
+test('both original function and wrapper functionality are run in the hook', () => {
+  const handleSubmit = jest.fn();
+  const functionToWrap = jest.fn().mockReturnValue({
+    handleSubmit,
+  });
+  const useFlow = wrapUseSignIn(functionToWrap);
+  const { result } = renderHook(() => useFlow());
+  expect(functionToWrap).toHaveBeenCalledTimes(1);
+  jest.spyOn(mdl.context, 'setShopper');
+  jest.spyOn(mdl.publish, 'signIn');
+  result.current.handleSubmit();
+  expect(handleSubmit).toHaveBeenCalledTimes(1);
+  expect(mdl.context.setShopper).toHaveBeenCalledTimes(1);
+  expect(mdl.context.setShopper).toHaveBeenLastCalledWith({
+    shopperId: 'logged-in',
+  });
+  expect(mdl.publish.signIn).toHaveBeenCalledTimes(1);
+});
