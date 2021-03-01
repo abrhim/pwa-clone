68fbe12e02110e9f85bafa5018adee91547fca96
himmer <himmer@himmers-MacBook-Pro.local>
2021-03-01 13:13:33 -0600

    test: DATA-2408 test

--- test/hooks/useRecsData.test.js
+++ test/hooks/useRecsData.test.js
@@ -1,31 +1,32 @@
 import useRecsData from '../../lib/recommendations/hooks/useRecsData';
 import { renderHook, act } from '@testing-library/react-hooks';
 import { CMS } from '../../lib/recommendations/constants';
-import recsClient from '../recsClient';
+// import recsClient from '../recsClient';
 // import RecommendationsClient from '@magento/recommendations-js-sdk';
 
 // RecommendationsClient;
 // RecommendationsClient;
 // import React from 'react';
 
-jest.mock('magento-data-layer-sdk', () => ({
-  context: {
-    getStorefrontInstance: jest.fn().mockReturnValue({ environmentId: 'id' }),
-  },
-}));
+// jest.mock('magento-data-layer-sdk', () => ({
+//   context: {
+//     getStorefrontInstance: jest.fn().mockReturnValue({ environmentId: 'id' }),
+//   },
+// }));
 
 // jest.mock('@magento/recommendations-js-sdk', () => {});
-window.RecommendationsClient = recsClient;
+// window.RecommendationsClient = recsClient;
 
 test('useRecsData returns fetched recs', () => {
-  let { result } = renderHook(props => act(useRecsData(props)), {
+  //   useRecsData;
+  renderHook(props => act(useRecsData(props)), {
     initialProps: { pageType: CMS },
   });
-  console.log(result.error);
+  //   console.log(result.error);
   expect(true).toBe(true);
 });
 
-test('useRecsData throws error without pageType', () => {
-  expect(() => renderHook(() => act(() => useRecsData()))).toThrow();
-  expect(true).toBe(true);
-});
+// test('useRecsData throws error without pageType', () => {
+//   expect(() => renderHook(() => act(() => useRecsData()))).toThrow();
+//   expect(true).toBe(true);
+// });
