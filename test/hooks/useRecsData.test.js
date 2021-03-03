192fc09885765c78720d8aba00d5b28734c7d1aa
himmer <himmer@himmers-MacBook-Pro.local>
2021-03-03 13:19:11 -0600

    test: DATA-2408 wrote the test

--- test/hooks/useRecsData.test.js
+++ test/hooks/useRecsData.test.js
@@ -1,32 +1,69 @@
 import useRecsData from '../../lib/recommendations/hooks/useRecsData';
 import { renderHook, act } from '@testing-library/react-hooks';
-import { CMS } from '../../lib/recommendations/constants';
-// import recsClient from '../recsClient';
-// import RecommendationsClient from '@magento/recommendations-js-sdk';
+import { fetchedRecs } from '../mocks';
+import { PageTypes, CMS } from '../../lib/recommendations/constants';
 
-// RecommendationsClient;
-// RecommendationsClient;
-// import React from 'react';
-
-// jest.mock('magento-data-layer-sdk', () => ({
-//   context: {
-//     getStorefrontInstance: jest.fn().mockReturnValue({ environmentId: 'id' }),
-//   },
-// }));
-
-// jest.mock('@magento/recommendations-js-sdk', () => {});
-// window.RecommendationsClient = recsClient;
-
-test('useRecsData returns fetched recs', () => {
-  //   useRecsData;
-  renderHook(props => act(useRecsData(props)), {
-    initialProps: { pageType: CMS },
-  });
-  //   console.log(result.error);
-  expect(true).toBe(true);
+const mockFetchPreconfigured = jest.fn();
+jest.mock('@magento/recommendations-js-sdk', () => {
+  return jest
+    .fn()
+    .mockImplementation(() => ({ fetchPreconfigured: mockFetchPreconfigured }));
+  //   return  fetchPreconfigured: jest.fn() };
 });
 
-// test('useRecsData throws error without pageType', () => {
-//   expect(() => renderHook(() => act(() => useRecsData()))).toThrow();
-//   expect(true).toBe(true);
-// });
+jest.mock('magento-data-layer-sdk', () => ({
+  context: {
+    getStorefrontInstance: jest.fn().mockReturnValue({ environmentId: 'id' }),
+  },
+}));
+
+it('useRecsData returns fetched recs', async () => {
+  await act(async () => {
+    mockFetchPreconfigured.mockReturnValue(fetchedRecs);
+    try {
+      const { result, waitForNextUpdate } = renderHook(() =>
+        useRecsData({ pageType: CMS }),
+      );
+
+      await waitForNextUpdate();
+      await waitForNextUpdate();
+      expect(result.current).toEqual({
+        data: fetchedRecs.data,
+        isLoading: false,
+        error: null,
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  });
+});
+
+it('useRecsData throws error without pageType', () => {
+  const {
+    result: { error },
+  } = renderHook(() => {
+    act(useRecsData());
+  });
+
+  expect(error).toEqual(
+    Error(
+      'Headless Recommendations: PageType is required to fetch recommendations.',
+    ),
+  );
+});
+
+it('useRecsData throws error with an incorrect pagetype', () => {
+  const {
+    result: { error },
+  } = renderHook(() => {
+    act(useRecsData({ pageType: 'CSM' }));
+  });
+
+  expect(error).toEqual(
+    Error(
+      `Headless Recommendations: CSM is not a valid pagetype. Valid types include ${JSON.stringify(
+        PageTypes,
+      )}`,
+    ),
+  );
+});
